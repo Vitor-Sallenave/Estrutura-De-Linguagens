26af306d8d2bb3b15ab87905c516cf0560d291fe
Nome do aluno <alunouerj@gmail.com>
2024-04-05 00:51:59 -0300

    Changing the examples between filter and map

--- Haskell/Questionários/3.3.2/map.hs
+++ Haskell/Questionários/3.3.2/map.hs
@@ -1,7 +1,21 @@
--- 1. Produtos corrigidos com a inflação
+-- 1. Análise das notas de alunos
+
+-- Médias finais dos alunos
+-- nota >= 7 (Aprovado)
+-- 5 <= nota < 7 (Em recuperação) 
+-- nota < 5 (reprovado)
+notas = [7.8, 9.0, 10.0, 2.8, 4.7, 5.0, 5.3]
+analisaNota nota
+  | nota >= 7.0 = "Aprovado"
+  | nota >= 5 = "Em recuperacao"
+  | otherwise = "Reprovado"
+
+resultados = map analisaNota notas
+
+-- 2. Produtos corrigidos com a inflação
 
 precos = [10, 20, 30, 40, 50]
 i = 0.2
 precosCorrigidos = map(\x -> x*(1+i)) precos
 
-main = print precosCorrigidos
+main = print (resultados, precosCorrigidos)
