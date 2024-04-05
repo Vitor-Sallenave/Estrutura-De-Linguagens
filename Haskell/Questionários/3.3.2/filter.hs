9cb74a577a929eb63a772261d71fea3e4fc22663
Nome do aluno <alunouerj@gmail.com>
2024-04-05 01:00:26 -0300

    Selecting candidates

--- Haskell/Questionários/3.3.2/filter.hs
+++ Haskell/Questionários/3.3.2/filter.hs
@@ -1,5 +1,8 @@
--- 1.
+-- 1. Corte de notas e seleção de candidatos em um processo seletivo
 
--- 2. Corte de notas em um processo seletivo
-
-main = print resultados
+notas = [701, 343, 762,
+        580, 900, 694,
+        568, 789, 873]
+corte = 760
+selecao = filter (> corte) notas
+main = print selecao
